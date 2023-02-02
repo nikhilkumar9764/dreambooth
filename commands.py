8913e74fb49d5f574231b7e72a9be78942557e8b
niranjansenthilkumar <ns633@cornell.edu>
2023-02-01 23:40:09 -0800

    dreambooth ananya

--- commands.py
+++ commands.py
@@ -21,18 +21,23 @@
 #     -H "Content-Type: application/json" \
 #     -d '{
 #             "input": {
-#                 "instance_prompt": "a photo of a xyz person",
+#                 "instance_prompt": "a photo of a cyz",
 #                 "class_prompt": "a photo of a person",
 #                 "instance_data": "'"$SERVING_URL"'",
 #                 "max_train_steps": 2000,
 #                 "ckpt_base": "https://huggingface.co/prompthero/openjourney-v2/resolve/main/openjourney-v2.ckpt"
 #             },
-#             "model": "niranjansenthilkumar/dreambooth_xyz",
-#             "trainer_version": "cd3f925f7ab21afaef7d45224790eedbb837eeac40d22e8fefe015489ab644aa",
+#             "model": "niranjansenthilkumar/dreambooth_cyz",
+#             "trainer_version": "9c41656f8ae2e3d2af4c1b46913d7467cd891f2c1c5f3d97f1142e876e63ed7a",
 #             "webhook_completed": "https://example.com/dreambooth-webhook"
 #         }' \
 #     https://dreambooth-api-experimental.replicate.com/v1/trainings
 
+
+
+RESPONSE=$(curl -X POST -H "Authorization: Token $REPLICATE_API_TOKEN" https://dreambooth-api-experimental.replicate.com/v1/upload/data.zip)
+curl -X PUT -H "Content-Type: application/zip" --upload-file data.zip "$(jq -r ".upload_url" <<< "$RESPONSE")"
+SERVINGG_URL=$(jq -r ".serving_url" <<< $RESPONSE)
 
 
 
@@ -71,3 +76,8 @@
 #             "webhook_completed": "https://example.com/dreambooth-webhook"
 #         }' \
 #     https://dreambooth-api-experimental.replicate.com/v1/trainings
+
+
+
+curl -H "Authorization: Token $REPLICATE_API_TOKEN" \
+  https://dreambooth-api-experimental.replicate.com/v1/trainings/n6uuvbwehfeudfvzgvbry5dm6y
