bffe13dcb168ab58957f114ba166d4e34b0340d6
niranjansenthilkumar <ns633@cornell.edu>
2023-02-01 22:34:29 -0800

    adding ananya dreambooth

--- commands.py
+++ commands.py
@@ -21,6 +21,27 @@
 #     -H "Content-Type: application/json" \
 #     -d '{
 #             "input": {
+#                 "instance_prompt": "a photo of a xyz person",
+#                 "class_prompt": "a photo of a person",
+#                 "instance_data": "'"$SERVING_URL"'",
+#                 "max_train_steps": 2000,
+#                 "ckpt_base": "https://huggingface.co/prompthero/openjourney-v2/resolve/main/openjourney-v2.ckpt"
+#             },
+#             "model": "niranjansenthilkumar/dreambooth_xyz",
+#             "trainer_version": "cd3f925f7ab21afaef7d45224790eedbb837eeac40d22e8fefe015489ab644aa",
+#             "webhook_completed": "https://example.com/dreambooth-webhook"
+#         }' \
+#     https://dreambooth-api-experimental.replicate.com/v1/trainings
+
+
+
+
+
+# curl -X POST \
+#     -H "Authorization: Token $REPLICATE_API_TOKEN" \
+#     -H "Content-Type: application/json" \
+#     -d '{
+#             "input": {
 #                 "instance_prompt": "a photo of a brb",
 #                 "class_prompt": "a photo of a bird",
 #                 "instance_data": "'"$SERVING_URL"'",
@@ -34,19 +55,19 @@
 #     https://dreambooth-api-experimental.replicate.com/v1/trainings
 
 
-curl -X POST \
-    -H "Authorization: Token $REPLICATE_API_TOKEN" \
-    -H "Content-Type: application/json" \
-    -d '{
-            "input": {
-                "instance_prompt": "a photo of a brb",
-                "class_prompt": "a photo of a bird",
-                "instance_data": "'"$SERVING_URL"'",
-                "max_train_steps": 2000,
-                "ckpt_base": "https://huggingface.co/prompthero/openjourney-v2/resolve/main/openjourney-v2.ckpt"
-            },
-            "model": "niranjansenthilkumar/dreambooth_brb2",
-            "trainer_version": "9c41656f8ae2e3d2af4c1b46913d7467cd891f2c1c5f3d97f1142e876e63ed7a",
-            "webhook_completed": "https://example.com/dreambooth-webhook"
-        }' \
-    https://dreambooth-api-experimental.replicate.com/v1/trainings
+# curl -X POST \
+#     -H "Authorization: Token $REPLICATE_API_TOKEN" \
+#     -H "Content-Type: application/json" \
+#     -d '{
+#             "input": {
+#                 "instance_prompt": "a photo of a brb",
+#                 "class_prompt": "a photo of a bird",
+#                 "instance_data": "'"$SERVING_URL"'",
+#                 "max_train_steps": 2000,
+#                 "ckpt_base": "https://huggingface.co/prompthero/openjourney-v2/resolve/main/openjourney-v2.ckpt"
+#             },
+#             "model": "niranjansenthilkumar/dreambooth_brb2",
+#             "trainer_version": "9c41656f8ae2e3d2af4c1b46913d7467cd891f2c1c5f3d97f1142e876e63ed7a",
+#             "webhook_completed": "https://example.com/dreambooth-webhook"
+#         }' \
+#     https://dreambooth-api-experimental.replicate.com/v1/trainings
--- notes.md
+++ notes.md
@@ -1,16 +1,24 @@
 like reaction:
+cute sticker of cjw person smiling, happy, satisfied, chibi style, kawaii, cartoon, goofy, vector
 
 love reaction:
 
+
 haha reaction:
+cute sticker of cjw person laughing, roaring with laughter, a jolly expression, chibi style, kawaii, cartoon, goofy, vector
+
+(negative prompts)
+multiple stickers, ((out of frame)), 3d, multiple heads, deformed, weird teeth
+
 
 wow reaction:
 
+
 sad reaction:
+cute sticker of cjw person crying, heartbroken, a sad expression, chibi style, kawaii, cartoon, goofy, vector
 
 angry reaction:
-cute sticker of cjw person yelling, furious, an angry expression, chibi style, kawaii, cartoon, goofy, vector, rule of thirds, golden ratio, perfect composition
-
+cute sticker of cjw person yelling, furious, an angry expression, chibi style, kawaii, cartoon, goofy, vector
 
 negative prompts:
-multiple stickers, ((out of frame)), 3d, multiple heads, deformed
+multiple stickers, ((out of frame)), 3d, multiple heads, deformed, weird teeth
