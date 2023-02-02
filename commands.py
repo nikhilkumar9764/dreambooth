7386453784256c7f6494ba52b372d35b9e319690
niranjansenthilkumar <ns633@cornell.edu>
2023-02-01 18:26:10 -0800

    adding prompts for angry

--- commands.py
+++ commands.py
@@ -50,3 +50,10 @@ curl -X POST \
             "webhook_completed": "https://example.com/dreambooth-webhook"
         }' \
     https://dreambooth-api-experimental.replicate.com/v1/trainings
+
+
+
+# cute sticker of cjw person yelling, furious, an angry expression, chibi style, kawaii, cartoon, goofy, vector, rule of thirds, golden ratio, perfect composition
+
+# negative prompts
+# multiple stickers, ((out of frame)), 3d, multiple heads, deformed, bad artist, bad prompt
