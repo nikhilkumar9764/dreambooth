df8871438715fb04d2f7be91dade8d46de9e8e6b
niranjansenthilkumar <ns633@cornell.edu>
2023-02-07 21:44:25 -0800

    testing out more configurations

--- commands.py
+++ commands.py
@@ -1,3 +1,41 @@
+
+# curl -X POST \
+#     -H "Authorization: Token $REPLICATE_API_TOKEN" \
+#     -H "Content-Type: application/json" \
+#     -d '{
+#             "input": {
+#                 "instance_prompt": "a photo of a cjw man",
+#                 "class_prompt": "a photo of a man",
+#                 "instance_data": "'"$SERVING_URL"'",
+#                 "max_train_steps": 2000,
+#                 "ckpt_base": "https://huggingface.co/prompthero/openjourney-v2/resolve/main/openjourney-v2.ckpt"
+#             },
+#             "model": "niranjansenthilkumar/dreambooth_nj_midjourney",
+#             "trainer_version": "9c41656f8ae2e3d2af4c1b46913d7467cd891f2c1c5f3d97f1142e876e63ed7a",
+#             "webhook_completed": "https://example.com/dreambooth-webhook"
+#         }' \
+#     https://dreambooth-api-experimental.replicate.com/v1/trainings
+
+
+# curl -X POST \
+#     -H "Authorization: Token $REPLICATE_API_TOKEN" \
+#     -H "Content-Type: application/json" \
+#     -d '{
+#             "input": {
+#                 "instance_prompt": "a photo of a cjw woman",
+#                 "class_prompt": "a photo of a woman",
+#                 "instance_data": "'"$SERVING_URL"'",
+#                 "max_train_steps": 2000,
+#                 "ckpt_base": "https://huggingface.co/prompthero/openjourney-v2/resolve/main/openjourney-v2.ckpt"
+#             },
+#             "model": "niranjansenthilkumar/dreambooth_ananya_openjourneyv2",
+#             "trainer_version": "9c41656f8ae2e3d2af4c1b46913d7467cd891f2c1c5f3d97f1142e876e63ed7a",
+#             "webhook_completed": "https://example.com/dreambooth-webhook"
+#         }' \
+#     https://dreambooth-api-experimental.replicate.com/v1/trainings
+
+
+
 # curl -X POST \
 #     -H "Authorization: Token $REPLICATE_API_TOKEN" \
 #     -H "Content-Type: application/json" \
